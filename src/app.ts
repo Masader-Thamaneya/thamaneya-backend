775cd7b7a4ca0cc66aa9a441115e82ac9e8df247
AhmedAAN <ahmed.a.nagy@outlook.com>
2025-08-02 17:46:26 +0300

    hotfix

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
-import { authenticate } from "./middlewares/auth";
 import { errorHandler } from "./middlewares/errorHandler";
-import reportsRoutes from "./routes/reports.routes";
-import authRoutes from "./routes/auth.routes";
-import compnayRoutes from "./routes/company.routes";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use("/auth", authRoutes);
-app.use("/cfp/reports", reportsRoutes);
-app.use("/companies", compnayRoutes);
-
 app.get("/", (req, res) => {
   res.send("Hello from root get");
 });
 
+app.use("/api", routes);
 app.use(errorHandler);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import reportsRoutes from "./reports.routes";
 import authRoutes from "./auth.routes";
 import compnayRoutes from "./company.routes";
+import { authenticate } from "../middlewares/auth";
 
 const router = Router();
 router.get("/", (req, res) => {
   res.send("haha");
 });
 
-router.use("/cfp/reports", reportsRoutes);
+router.use("/cfp/reports", authenticate, reportsRoutes);
 router.use("/auth", authRoutes);
-router.use("/companies", compnayRoutes);
+router.use("/companies", authenticate, compnayRoutes);
 
 export default router;
