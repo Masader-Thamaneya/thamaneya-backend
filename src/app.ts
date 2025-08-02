6cae84ece3e91bd3375c605d7efaf6fddfb58e99
AhmedAAN <ahmed.a.nagy@outlook.com>
2025-08-02 21:34:20 +0300

    hotfix

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
 import { errorHandler } from "./middlewares/errorHandler";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // or "*" for testing
+    credentials: true, // allow cookies / Authorization headers
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from root get");
