b4fe5a6846499dfff7d58605f1aa175567769931
AhmedAAN <ahmed.a.nagy@outlook.com>
2025-08-02 17:36:50 +0300

    build command added

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,23 @@ import dotenv from "dotenv";
 import routes from "./routes";
 import { authenticate } from "./middlewares/auth";
 import { errorHandler } from "./middlewares/errorHandler";
+import reportsRoutes from "./routes/reports.routes";
+import authRoutes from "./routes/auth.routes";
+import compnayRoutes from "./routes/company.routes";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+app.use("/cfp/reports", reportsRoutes);
+app.use("/companies", compnayRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello from root get");
 });
 
-app.use(authenticate, routes);
 app.use(errorHandler);
 
 export default app;
