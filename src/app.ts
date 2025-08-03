94f4a6d42be59f572513abdbeadb39856550da7d
AhmedAAN <ahmed.a.nagy@outlook.com>
2025-08-03 15:27:51 +0300

    cors allowed for everyone

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // or "*" for testing
-    credentials: true, // allow cookies / Authorization headers
+    origin: "*", // or "*" for testing
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
