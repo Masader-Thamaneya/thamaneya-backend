07bfbf6d19a62f9212d6f176d70a5d4df4eab78c
AhmedAAN <ahmed.a.nagy@outlook.com>
2025-08-07 12:47:35 +0300

    preflight request handled

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -21,7 +21,10 @@ app.use(
 
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
+    res.header("Access-Control-Allow-Origin", "*"); // or specific domain
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return res.sendStatus(204); // No Content
   }
   next();
 });
